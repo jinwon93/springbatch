e8489e213702b04ae0283735cb21a1bde8055aba
jinwon93 <kjw8208@nate.com>
2023-03-18 21:26:22 +0900

    springbatch

---
+++ src/main/java/com/example/springbatch/HellowJobConfiguration.java
@@ -0,0 +1,58 @@
+package com.example.springbatch;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class HellowJobConfiguration {
+
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+
+    @Bean
+    public Job hellowJob(){
+        return jobBuilderFactory.get("hellow")
+                .start(hellowStep1())
+                .next(hellowStep2())
+                .build();
+    }
+
+
+    @Bean
+    public Step hellowStep1(){
+        return stepBuilderFactory.get("hellowStep1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+
+    @Bean
+    public Step hellowStep2(){
+        return stepBuilderFactory.get("hellowStep2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+}

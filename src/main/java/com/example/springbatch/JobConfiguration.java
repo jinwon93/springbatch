3ce87a1c2f7bd45e5068fe44273d3272a4c7d815
redsoftjin <kjw8208@nate.com>
2023-04-03 18:45:15 +0900

    20230403

---
+++ src/main/java/com/example/springbatch/JobConfiguration.java
@@ -0,0 +1,64 @@
+package com.example.springbatch;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.boot.autoconfigure.batch.JobLauncherApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class JobConfiguration {
+
+
+    private final JobLauncherApplicationRunner jobLauncherApplicationRunner;
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+
+    @Bean
+    public Job job(){
+        return jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step1(){
+        return stepBuilderFactory.get("hellowStep1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+
+    @Bean
+    public Step step2(){
+        // jobInstance B ==> JobExecution status가 failed면 JobExecution에 저장
+        return stepBuilderFactory.get("hellowStep2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+
+                        throw  new RuntimeException("step2 hjas faild");
+//                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+}

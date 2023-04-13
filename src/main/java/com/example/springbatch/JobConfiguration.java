960ff7137ef30c8ea3684ad4d5c8d4674202a204
jinwon93 <kjw8208@nate.com>
2023-04-13 20:52:41 +0900

    20230413

--- src/main/java/com/example/springbatch/JobConfiguration.java
+++ src/main/java/com/example/springbatch/JobConfiguration.java
@@ -7,13 +7,16 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.autoconfigure.batch.JobLauncherApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+
 
 
 @Configuration
@@ -34,6 +37,17 @@ public class JobConfiguration {
                 .build();
     }
 
+
+    @Bean
+    public Job jobFlow(){
+        return this.jobBuilderFactory.get("job")
+                .start(flow())
+                .next(step5())
+                .end()
+                .build();
+
+    }
+
     @Bean
     public Step step1(){
         return stepBuilderFactory.get("hellowStep1")
@@ -51,6 +65,63 @@ public class JobConfiguration {
     public Step step2(){
         // jobInstance B ==> JobExecution status가 failed면 JobExecution에 저장
         return stepBuilderFactory.get("hellowStep2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+
+                        throw  new RuntimeException("step2 hjas faild");
+//                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    public Flow flow() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+
+        flowBuilder.start(step3())
+                .next(step4())
+                .end();
+
+        return flowBuilder.build();
+
+    }
+
+    @Bean
+    public Step step3(){
+        // jobInstance B ==> JobExecution status가 failed면 JobExecution에 저장
+        return stepBuilderFactory.get("hellowStep3")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+
+                        throw  new RuntimeException("step2 hjas faild");
+//                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+
+    @Bean
+    public Step step4(){
+        // jobInstance B ==> JobExecution status가 failed면 JobExecution에 저장
+        return stepBuilderFactory.get("hellowStep4")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+
+                        throw  new RuntimeException("step2 hjas faild");
+//                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step5(){
+        // jobInstance B ==> JobExecution status가 failed면 JobExecution에 저장
+        return stepBuilderFactory.get("hellowStep4")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {

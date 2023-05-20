b8f845c604fe3e372a650360d002d28369261eda
jinwon93 <kjw8208@nate.com>
2023-05-20 12:03:52 +0900

    20230520

--- src/main/java/com/example/springbatch/FlatFilesFixedLengthConfiguration.java
+++ src/main/java/com/example/springbatch/FlatFilesFixedLengthConfiguration.java
@@ -8,23 +8,21 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemStreamWriter;
-import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.Range;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-
-import java.util.Arrays;
+import org.springframework.core.io.FileSystemResource;
 
 
 @Configuration
 @RequiredArgsConstructor
 // ItmeReader ItemWriter ItemProcessor
-public class FlatFilesConfiguration {
+public class FlatFilesFixedLengthConfiguration {
 
 
     private final StepBuilderFactory stepBuilderFactory;
@@ -56,17 +54,21 @@ public class FlatFilesConfiguration {
                 .build();
     }
 
-    public ItemReader itemReader(){
-
-        return  new FlatFileItemReaderBuilder<Customer>()
-                .name("flatFile")
-                .resource(new ClassPathResource("/customer.csv"))
+    public FlatFileItemReader itemReader(){
+        return new FlatFileItemReaderBuilder<Customer>()
+                .name("flatfile")
+                .resource(new FileSystemResource("C:\\Users\\USER\\Downloads\\spring-batch\\springbatch\\src\\main\\resources\\customer.csv"))
                 .fieldSetMapper(new BeanWrapperFieldSetMapper<>())
                 .targetType(Customer.class)
                 .linesToSkip(1)
-                .delimited().delimiter(",")
-                .names("name" ,"age" , "year")
+                .fixedLength()
+                .addColumns(new Range(1,5))
+                .addColumns(new Range(6 , 9))
+                .addColumns(new Range(10 , 11))
+                .names("name" , "year" ,"age")
                 .build();
+
+
     }
 
 
